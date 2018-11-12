486df16871c44f55105cdb7bd1e097ecbab76575
Virginia Dooley <vcdooley@gmail.com>
2018-11-12 14:44:36 +0000

    changed url, updated gemspec with repo

--- lib/podcastme/cli.rb
+++ lib/podcastme/cli.rb
@@ -1,28 +1,31 @@
 class CLI
   podcast_titles = []
+  podcast_summaries = []
+  podcast_urls = []
+
+
   def run
     puts "welcome to podcastme: find your next podcast"
-  end
 
-  html = open('https://www.stitcher.com/stitcher-list/all-podcasts-top-shows')
 
+  html = open('https://www.vulture.com/2018/10/best-podcasts-2018-so-far.html')
   doc = Nokogiri::HTML(html)
+  binding.pry
 
-    doc.css(".stitcher-list tr")
-      .each do |podcast|
-        title = podcast.css(".sl-showName").text
-        genre = podcast.css(".sl-category").text
-        rank = podcast.css(".sl-rank").text
-        url = title = podcast.css(".sl-showName a href")
-        podcast_titles << title
-        podcast_genres << genre
-        podcast_ranks << rank
-        podcast_url << url 
-      end
-
-      podcast_titles.each do |title|
-        puts "#{title}"
-        puts
-      end
+    doc.css(".content").each do |podcast|
+      title = podcast.css(".clay-subheader").text.strip
+      summary = podcast.css(".clay-paragraph").text
+      url = podcast.css(".a href")
+      podcast_titles << title
+      podcast_summaries << summary
+      podcast_urls << url
+    end
+      #
+    podcast_titles.each do |title|
+      puts "#{title}".chomp
+      puts "#{summary}".chomp
+      puts "#{url}".chomp
+    end
+  end
 
 end
