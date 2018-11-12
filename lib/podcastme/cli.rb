416b8b722c7b8e858714319232434b337716766e
Virginia Dooley <vcdooley@gmail.com>
2018-11-12 13:05:32 +0000

    iterate to dispplay titles

--- lib/podcastme/cli.rb
+++ lib/podcastme/cli.rb
@@ -1,5 +1,5 @@
 class CLI
-
+  podcast_titles = []
   def run
     puts "welcome to podcastme: find your next podcast"
   end
@@ -7,11 +7,16 @@ class CLI
   html = open('https://www.vulture.com/2018/10/best-podcasts-2018-so-far.html')
 
   doc = Nokogiri::HTML(html)
-  binding.pry
-
 
     doc.css(".content")
       .each do |podcast|
+        title = podcast.css(".clay-subheader").text.strip
+        #need to seperate producers from titles
+        podcast_titles << title
+      end
+
+      podcast_titles.each do |title|
+        puts "#{title}"
       end
 
 end
