538d8ac40f577acf243c58b5bc655cfb3f06ec0b
Virginia Dooley <vcdooley@gmail.com>
2018-11-27 20:03:11 +0000

    moved the requires, name spaced in module podcastme

--- lib/podcastme/scraper.rb
+++ lib/podcastme/scraper.rb
@@ -1,18 +1,22 @@
- class Scraper
+require 'nokogiri'
+require 'open-uri'
 
-  def self.scrape_podcast
+module Podcastme
+   class Scraper
 
-    doc = Nokogiri::HTML(open('https://www.vulture.com/2018/10/best-podcasts-2018-so-far.html'))
+    def self.scrape_podcast
 
-    doc.css(".content").each do |podcast_doc|
-      title = podcast_doc.css(".clay-subheader").text.strip.split( " (" )[0]
-      producer = podcast_doc.css(".clay-subheader").text.strip.split(" (" )[1].chop
-      url = podcast_doc.css("a").attr("href").value
-      summary = podcast_doc.css(".clay-paragraph").text
+      doc = Nokogiri::HTML(open('https://www.vulture.com/2018/10/best-podcasts-2018-so-far.html'))
 
-      Podcast.new(title, producer, url, summary)
+      doc.css(".content").each do |podcast_doc|
+        title = podcast_doc.css(".clay-subheader").text.strip.split( " (" )[0]
+        producer = podcast_doc.css(".clay-subheader").text.strip.split(" (" )[1].chop
+        url = podcast_doc.css("a").attr("href").value
+        summary = podcast_doc.css(".clay-paragraph").text
 
+        Podcast.new(title, producer, url, summary)
+
+      end
     end
   end
-
 end
