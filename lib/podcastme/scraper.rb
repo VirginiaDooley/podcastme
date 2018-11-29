c3cd9b079bc1a182db84449b2395445082962427
Virginia Dooley <vcdooley@gmail.com>
2018-11-29 09:52:51 +0000

    recreated instance variable podcasts and shoveled in all new instances of Podcast

--- lib/podcastme/scraper.rb
+++ lib/podcastme/scraper.rb
@@ -1,6 +1,7 @@
- class Scraper
-
+class Scraper
   def self.scrape_podcast
+
+    podcasts = []
 
     doc = Nokogiri::HTML(open('https://www.vulture.com/2018/10/best-podcasts-2018-so-far.html'))
 
@@ -10,9 +11,10 @@
       url = podcast_doc.css("a").attr("href").value
       summary = podcast_doc.css(".clay-paragraph").text
 
-      Podcast.new(title, producer, url, summary)
-
+      podcasts << Podcast.new(title, producer, url, summary)
     end
+
+    podcasts
   end
 
 end
